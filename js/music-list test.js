40d3286023c9923667cff22d3996acc8c6afae7c
Felix Oliveira <felixoliveira.dev@gmail.com>
2022-01-27 20:20:20 -0300

    first commit

---
+++ js/music-list test.js
@@ -0,0 +1,66 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Eu Te Devoro",
+    artist: "Djavan",
+    img: "music-1",
+    src: "https://www.igm.mat.br/igmantigo/hp-antigo/2010-1/cd-mpb1/dijavan%20-%20te%20devoro.mp3"
+  },
+  
+  {
+    name: "Palavras ao vento ",
+    artist: "Cassia Eller",
+    img: "music-2",
+    src: "http://peterewendy.com.br/Genero/Mp3//NOVELAS_ANOS_2000_(_A_-_B_)/ALEM_DO_TEMPO_-_Cassia_Eller_-_Palavras_Ao_Vento_-_P&W_-_P&W.mp3"
+  },
+  {
+    name: "Tempo Perdido",
+    artist: "Legião Urbana ",
+    img: "music-2",
+    src: "http://peterewendy.com.br/Genero/Mp3//FESTA_PLOC/Legiao_Urbana_-_Tempo_Perdido_-_P&W.mp3"
+  },
+  {
+    name: ' "Pra Dizer Adeus"',
+    artist: "Titãs ",
+    img: "music-2",
+    src: "http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_L_-_Z_)/Titas_-_Pra_Dizer_Adeus_-_P&W.mp3"
+  },
+  {
+    name: ' Exagerado',
+    artist: "Cazuza",
+    img: "music-2",
+    src: "http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_A_-_K_)/Cazuza_-_Exagerado_-_P&W.mp3"
+  },
+  {
+    name: ' Segredos',
+    artist: "Frejat",
+    img: "music-2",
+    src: "http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_A_-_K_)/Frejat_-_Segredos_-_P&W.mp3"
+  },
+  {
+    name: ' Amado',
+    artist: "Vanessa Da Mata ",
+    img: "music-2",
+    src: "http://peterewendy.com.br/Genero/Mp3//MPB_(_P_-_Z_)/Vanessa_Da_Mata_-_Amado_-_P&W.mp3"
+  },
+  {
+    name: '  Oceano',
+    artist: "Djavana ",
+    img: "music-2",
+    src: "http://peterewendy.com.br/Genero/Mp3//MPB_(_A_-_D_)/Djavan_-_Oceano_-_P&W.mp3"
+  },
+
+ 
+];
+
+
---
+++ js/music-list.js
@@ -0,0 +1,116 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Eu Te Devoro",
+    artist: "Djavan",
+    img: "music-2",
+    src: "https://www.igm.mat.br/igmantigo/hp-antigo/2010-1/cd-mpb1/dijavan%20-%20te%20devoro.mp3"
+  },
+  
+  {
+    name: "Palavras ao vento ",
+    artist: "Cassia Eller",
+    img: "music-2",
+    src: "http://peterewendy.com.br/Genero/Mp3//NOVELAS_ANOS_2000_(_A_-_B_)/ALEM_DO_TEMPO_-_Cassia_Eller_-_Palavras_Ao_Vento_-_P&W_-_P&W.mp3"
+  },
+  {
+    name: "Tempo Perdido",
+    artist: "Legião Urbana ",
+    img: "music-2",
+    src: "http://peterewendy.com.br/Genero/Mp3//FESTA_PLOC/Legiao_Urbana_-_Tempo_Perdido_-_P&W.mp3"
+  },
+  {
+    name: ' "Pra Dizer Adeus"',
+    artist: "Titãs ",
+    img: "music-2",
+    src: "http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_L_-_Z_)/Titas_-_Pra_Dizer_Adeus_-_P&W.mp3"
+  },
+  {
+    name: ' Exagerado',
+    artist: "Cazuza",
+    img: "music-2",
+    src: "http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_A_-_K_)/Cazuza_-_Exagerado_-_P&W.mp3"
+  },
+  {
+    name: ' Segredos',
+    artist: "Frejat",
+    img: "music-2",
+    src: "http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_A_-_K_)/Frejat_-_Segredos_-_P&W.mp3"
+  },
+  {
+    name: ' Amado',
+    artist: "Vanessa Da Mata ",
+    img: "music-2",
+    src: "http://peterewendy.com.br/Genero/Mp3//MPB_(_P_-_Z_)/Vanessa_Da_Mata_-_Amado_-_P&W.mp3"
+  },
+  {name: '  Oceano', artist: "Djavana ",  img: "music-2",  src: "http://peterewendy.com.br/Genero/Mp3//MPB_(_A_-_D_)/Djavan_-_Oceano_-_P&W.mp3"},
+
+
+{name: 'Resposta',artist: 'Skank',img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_L_-_Z_)/Skank_-_Resposta_-_P&W.mp3'},
+{name: 'Acima do Sol', artist: 'Skank', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_L_-_Z_)/Skank_-_Acima_Do_Sol_-_P&W.mp3'},
+{name: 'Quero Ser Feliz Também', artist: 'Natiruts', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//POP_NACIONAL/Natiruts_-_Quero_Ser_Feliz_Tambem_-_P&W.mp3'},
+{name: 'Pais e Filhos', artist: 'Legião Urbana', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_L_-_Z_)/Legiao_Urbana_-_Pais_E_Filhos_-_P&W.mp3'},
+{name: 'Codinome Beija-Flor', artist: 'cazuza', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_A_-_K_)/Cazuza_-_Codinome_Beija-Flor_-_P&W.mp3'},
+{name: 'Ideologia', artist: 'cazuza', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_A_-_K_)/Cazuza_-_Ideologia_-_P&W.mp3'},
+{name: 'O Tempo Não Para', artist: 'cazuza', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_A_-_K_)/Cazuza_-_O_Tempo_Nao_Para_-_P&W.mp3'},
+{name: 'Maior Abandonado', artist: 'cazuza', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_A_-_K_)/Cazuza_-_Maior_Abandonado_-_P&W.mp3'},
+{name: 'O Nosso Amor a Gente Inventa', artist: 'cazuza', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_A_-_K_)/Cazuza_-_O_Nosso_Amor_A_Gente_Inventa_-_P&W.mp3'},
+{name: 'Pro Dia Nascer Feliz', artist: 'cazuza', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_A_-_K_)/Cazuza_-_Pro_Dia_Nascer_Feliz_-_P&W.mp3'},
+{name: 'Todo O Amor Que Houver Nessa Vida', artist: 'cazuza', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_A_-_K_)/Cazuza_-_Todo_Amor_Que_Houver_Nessa_Vida_-_P&W.mp3'},
+{name: 'Vida Louca Vida', artist: 'cazuza', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_A_-_K_)/Cazuza_-_Vida_Louca_-_P&W.mp3'},
+{name: 'Ainda É Cedo', artist: 'Legião Urbana', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_L_-_Z_)/Legiao_Urbana_-_Ainda_E_Cedo_-_P&W.mp3'},
+{name: 'Eduardo e Mônica', artist: 'Legião Urbana', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_L_-_Z_)/Legiao_Urbana_-_Eduardo_E_Monica_-_P&W.mp3'},
+{name: 'Eu sei', artist: 'Legião Urbana', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_L_-_Z_)/Legiao_Urbana_-_Eu_Sei_-_P&W.mp3'},
+{name: 'Giz', artist: 'Legião Urbana', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_L_-_Z_)/Legiao_Urbana_-_Giz_-_P&W.mp3'},
+{name: 'Hoje a noite não tem luar', artist: 'Legião Urbana', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_L_-_Z_)/Legiao_Urbana_-_Hoje_A_Noite_Nao_Tem_Luar_-_P&W.mp3'},
+{name: 'Índios', artist: 'Legião Urbana', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_L_-_Z_)/Legiao_Urbana_-_Indios_-_P&W.mp3'},
+{name: 'Meninos e Meninas', artist: 'Legião Urbana', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_L_-_Z_)/Legiao_Urbana_-_Meninos_E_Meninas_-_P&W.mp3'},
+{name: 'Monte Castelo', artist: 'Legião Urbana', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_L_-_Z_)/Legiao_Urbana_-_Monte_Castelo_-_P&W.mp3'},
+{name: 'Quase Sem Querer', artist: 'Legião Urbana', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_L_-_Z_)/Legiao_Urbana_-_Quase_Sem_Querer_-_P&W.mp3'},
+{name: 'Poema', artist: 'Ney Matogrosso', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//MPB_(_L_-_O_)/Ney_Matogrosso_-_Poema_-_P&W.mp3'},
+{name: 'À Sua Maneira', artist: 'Capital Inicial', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_A_-_K_)/Capital_Inicial_-_A_Sua_Maneira_-_P&W.mp3'},
+{name: 'Algum Dia', artist: 'Capital Inicial', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_A_-_K_)/Capital_Inicial_-_Algum_Dia_-_P&W.mp3'},
+{name: 'Cai a Noite', artist: 'Capital Inicial', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_A_-_K_)/Capital_Inicial_-_Cai_A_Noite_-_P&W.mp3'},
+{name: 'Como Devia Estar', artist: 'Capital Inicial', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_A_-_K_)/Capital_Inicial_-_Como_Devia_Estar_-_P&W.mp3'},
+{name: 'Eu Nunca Disse Adeus', artist: 'Capital Inicial', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_A_-_K_)/Capital_Inicial_-_Eu_Nunca_Disse_Adeus_-_P&W.mp3'},
+{name: 'Primeiros Erros', artist: 'Capital Inicial', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_A_-_K_)/Capital_Inicial_-_Primeiros_Erros_-_P&W.mp3'},
+{name: 'Olhos Vermelhos', artist: 'Capital Inicial', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_A_-_K_)/Capital_Inicial_-_Olhos_Vermelhos_-_P&W.mp3'},
+{name: 'Alagados', artist: 'Paralamas do Sucesso', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//FESTA_PLOC/Paralamas_Do_Sucesso_-_Alagados_-_P&W.mp3'},
+{name: 'Meu Erro', artist: 'Paralamas do Sucesso', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//FESTA_PLOC/Paralamas_Do_Sucesso_-_Meu_Erro_-_P&W.mp3'},
+{name: 'Ela Disse Adeus', artist: 'Paralamas do Sucesso', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_L_-_Z_)/Paralamas_Do_Sucesso_-_Ela_Disse_Adeus_-_P&W.mp3'},
+{name: 'Aonde Quer Que Eu Va', artist: 'Paralamas do Sucesso', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_L_-_Z_)/Paralamas_Do_Sucesso_-_Aonde_Quer_Que_Eu_Va_-_P&W.mp3'},
+{name: 'Lanterna Dos Afogados', artist: 'Paralamas do Sucesso', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_L_-_Z_)/Paralamas_Do_Sucesso_-_Lanterna_Dos_Afogados_-_P&W.mp3'},
+{name: 'Noites De Um Verao Qualquer', artist: 'Skank', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_L_-_Z_)/Skank_-_Noites_De_Um_Verao_Qualquer_-_P&W.mp3'},
+{name: 'Sutilmente', artist: 'Skank', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_L_-_Z_)/Skank_-_Sutilmente_-_P&W.mp3'},
+{name: 'Simplesmente', artist: 'Skank', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_L_-_Z_)/Skank_-_Simplesmente_(feat._Roberta_Campos)_-_P&W.mp3'},
+{name: 'Vamos_Fugir', artist: 'Skank', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_L_-_Z_)/Skank_-_Vamos_Fugir_-_P&W.mp3'},
+{name: 'Vou Deixar', artist: 'Skank', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_L_-_Z_)/Skank_-_Vou_Deixar_-_P&W.mp3'},
+{name: 'Ainda Gosto Dela', artist: 'Skank', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_L_-_Z_)/Skank_-_Ainda_Gosto_Dela_-_P&W.mp3'},
+{name: 'Dois Rios', artist: 'Skank', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_L_-_Z_)/Skank_-_Dois_Rios_-_P&W.mp3'},
+{name: 'La Belle De Jour', artist: 'Alceu Valença', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//MPB_(_A_-_D_)/Alceu_Valenca_-_La_Belle_De_Jour_-_P&W.mp3'},
+{name: 'Morena Tropicana', artist: 'Alceu Valença', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//MPB_(_A_-_D_)/Alceu_Valenca_-_Morena_Tropicana_-_P&W.mp3'},
+{name: 'Na Primeira Manha', artist: 'Alceu Valença', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//MPB_(_A_-_D_)/Alceu_Valenca_-_Na_Primeira_Manha_-_P&W.mp3'},
+{name: 'Flor De Tangerina', artist: 'Alceu Valença', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//MPB_(_A_-_D_)/Alceu_Valenca_-_Flor_De_Tangerina_-_P&W.mp3'},
+{name: 'Ai,_Ai,_Ai', artist: 'Vanessa Da Mata', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//MPB_(_P_-_Z_)/Vanessa_Da_Mata_-_Ai,_Ai,_Ai_-_P&W.mp3'},
+{name: 'Amado', artist: 'Vanessa Da Mata', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//MPB_(_P_-_Z_)/Vanessa_Da_Mata_-_Amado_-_P&W.mp3'},
+{name: 'As Palavras', artist: 'Vanessa Da Mata', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//MPB_(_P_-_Z_)/Vanessa_Da_Mata_-_As_Palavras_-_P&W.mp3'},
+{name: 'Hoje Eu Sei', artist: 'Vanessa Da Mata', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//MPB_(_P_-_Z_)/Vanessa_Da_Mata_-_Hoje_Eu_Sei_-_P&W.mp3'},
+
+                                                                                         
+{name: 'Quem Me Leva Os Meus Fantasmas', artist: 'Maria Bethânia', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//*NANNY/Maria_Bethania_-_Quem_Me_Leva_Os_Meus_Fantasmas-nanny.mp3'},
+{name: 'Amor Perfeito', artist: 'Roberto Carlos', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//ROBERTO_CARLOS/Roberto_Carlos_-_Amor_Perfeito_-_P&W.mp3'},
+{name: 'Equalize', artist: 'Pitty', img: 'music-2', src: 'http://peterewendy.com.br/Genero/Mp3//POP_ROCK_NACIONAL_(_L_-_Z_)/Pitty_-_Equalize_-_P&W.mp3'}
+ 
+];
+
+
